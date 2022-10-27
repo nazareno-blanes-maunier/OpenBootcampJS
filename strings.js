ac0699838cea000b5158eb371eb27be10b99a7d7
Nazareno <maunier20@gmail.com>
2022-10-26 21:35:41 -0300

    strings

---
+++ strings.js
@@ -0,0 +1,13 @@
+let str_Nombre = "Nazareno";
+let str_Apellido = "Blanes-Maunier";
+let estudiante = str_Nombre + " " + str_Apellido;
+
+console.log(estudiante)
+
+let estudianteMayus = estudiante.toUpperCase
+
+let estudianteMinus = estudiante.toLowerCase
+
+console.log(estudianteMayus)
+
+console.log(estudianteMinus)
