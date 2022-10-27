f10aa5bbddab6e7ff510ef3f4c0439a3734306a7
Nazareno <maunier20@gmail.com>
2022-10-26 22:50:15 -0300

    numbers in JS

---
+++ numbers.js
@@ -0,0 +1,18 @@
+let altura_cm = 170;
+console.log(altura_cm);
+
+let altura_m = altura_cm.toFixed(Number) / 100;
+console.log(altura_m);
+
+let peso = 69.5;
+console.log(peso);
+
+let altura_m_redondeado = Math.round(altura_m);
+console.log(altura_m_redondeado);
+
+let peso_redondeado = Math.floor(peso);
+console.log(peso_redondeado);
+
+let boolean = false;
+let variable = Math.max(boolean) + 1;
+console.log(boolean);
