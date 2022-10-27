f819173115d85d259666a4abaad633e924c9ef29
Nazareno <maunier20@gmail.com>
2022-10-26 21:49:05 -0300

    changes upload

--- strings.js
+++ strings.js
@@ -2,12 +2,32 @@ let str_Nombre = "Nazareno";
 let str_Apellido = "Blanes-Maunier";
 let estudiante = str_Nombre + " " + str_Apellido;
 
-console.log(estudiante)
+console.log(estudiante);
 
-let estudianteMayus = estudiante.toUpperCase
+let estudianteMayus = estudiante.toUpperCase(estudiante);
+let estudianteMinus = estudiante.toLowerCase(estudiante);
 
-let estudianteMinus = estudiante.toLowerCase
+console.log(estudianteMayus);
+console.log(estudianteMinus);
 
-console.log(estudianteMayus)
+let numeroLetras = estudiante.length;
+let numeroLetrasApellido = str_Apellido.length;
 
-console.log(estudianteMinus)
+console.log(numeroLetrasApellido);
+console.log(numeroLetras);
+
+let primerLetra = str_Nombre.slice(0, 1);
+
+console.log(primerLetra);
+
+let ultimaLetra = str_Apellido.slice(13, 14);
+
+console.log(ultimaLetra);
+
+let eliminar = str_Nombre.trimEnd() + str_Apellido;
+
+console.log(eliminar);
+
+let boolean = estudiante.includes(str_Nombre)
+
+console.log(boolean)
